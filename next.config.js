21472c4566d0c37ae379b86f77ed8c9b5bf41fda
Doge2x <doge2x@outlook.com>
2023-03-19 19:46:17 +0800

    build(next): turn off react strict mode

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-const env = require("dotenv").config({
-  path: require("path").join(__dirname, ".env.local"),
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: env.parsed,
-  reactStrictMode: true,
   output: "standalone",
 };
 
