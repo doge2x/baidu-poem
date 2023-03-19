1d497ca60edde919e731c4a14840f5b091e444ff
doge2x <doge2x@163.com>
2023-03-19 20:24:38 +0800

    feat(api): use `swr` to fetch data

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <Flex w="100%" p="3" my="1" justify="center">
       <Text fontSize="lg" fontWeight="bold">
-        智能作诗
+        从言·寺声
       </Text>
     </Flex>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>智能作诗</title>
+        <title>从言·寺声</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
