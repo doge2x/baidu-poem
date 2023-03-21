ff9b05d6ab020145e9692215f685edfa23383515
Doge2x <doge2x@outlook.com>
2023-03-21 20:39:38 +0800

    feat(server): 支持自定义 basePath

--- lib/util.ts
+++ lib/util.ts
@@ -1,7 +1,10 @@
-export const newUrl = (url: string, params: object = {}): URL => {
+const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
+export const newUrl = (url: string, params: object = {}) => {
   let base;
   if (url.startsWith("/")) {
-    base = self.location.href;
+    url = BASE_PATH + url;
+    base = globalThis.location.href;
   }
   const ret = new URL(url, base);
   for (const [k, v] of Object.entries(params)) {
