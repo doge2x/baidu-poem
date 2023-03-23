f05c71ec9c680a052e5b32a45c530bb9e692fc2e
Doge2x <doge2x@outlook.com>
2023-03-23 13:07:46 +0800

    Revert "feat(server): 支持自定义 basePath"
    This reverts commit ec6bf74c4ab4c21c062ec85f070211fbac319792.

--- lib/util.ts
+++ lib/util.ts
@@ -1,10 +1,7 @@
-const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
-export const newUrl = (url: string, params: object = {}) => {
+export const newUrl = (url: string, params: object = {}): URL => {
   let base;
   if (url.startsWith("/")) {
-    url = BASE_PATH + url;
-    base = globalThis.location.href;
+    base = self.location.href;
   }
   const ret = new URL(url, base);
   for (const [k, v] of Object.entries(params)) {
